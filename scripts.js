d6af42cfe1a2d9a8703771e9f440d0fb411ba37f
Marcelo Alarcón <marceloalarconbarrenechea@gmail.com>
2023-01-16 17:22:32 -0400

    Add Localstorage

--- scripts.js
+++ scripts.js
@@ -1,56 +1,59 @@
 let localStorageBooks = []
-
-function addBook(cTitle, cAuthor) {
-    const newBook = {
-        title: cTitle,
-        author: cAuthor
-    };
-
-    localStorageBooks.push(newBook);
-    localStorage.setItem('localStorageBooks', JSON.stringify(localStorageBooks));
-
-    console.log(localStorageBooks);
-}
-
-function removeBook(cTitle) {
-    //const result = localStorageBooks.filter(book => book.title != cTitle);
-    //books = result;
-}
-//addBook('Try Again','bbb')
-//removeBook('Padre Rico Padre Pobre');
-
-/* -------------------------------------------------------------------------- */
-/*                              DOM MANIPULATION                              */
-/* -------------------------------------------------------------------------- */
 const div = document.querySelector('#bookListContainer')
 const ul = document.querySelector('#ulBookList')
-
-let aux = ''
-
-for (let i = 0; i < localStorageBooks.length; i++) {
-    aux += `<li>
-                <p>${localStorageBooks[i].title}</p>
-                <p>${localStorageBooks[i].author}</p>
-                <button id="btnRemove${i} type="button">Remove</button>
-                <hr />
-            </li>`
-
-}
-ul.innerHTML = aux;
-div.appendChild(ul)
-
 const addForm = document.getElementById('addBookForm')
-
-addForm.addEventListener('submit', ()=>{
-    addBook(addForm['title'].value, addForm['author'].value);
-    window.location.reload();
-});
 
 window.addEventListener('load', () => {
     if (JSON.parse(localStorage.getItem('localStorageBooks')) === null) {
         localStorage.setItem('localStorageBooks', JSON.stringify(localStorageBooks));
     } else {
         localStorageBooks = JSON.parse(localStorage.getItem('localStorageBooks'));
-        
     }
+
+    /* -------------------------------------------------------------------------- */
+    /*                              DOM MANIPULATION                              */
+    /* -------------------------------------------------------------------------- */
+
+
+    let aux = ''
+    for (let i = 0; i < localStorageBooks.length; i++) {
+        aux += `<li>
+                <p>${localStorageBooks[i].title}</p>
+                <p>${localStorageBooks[i].author}</p>
+                <button class="btnRemove" id="btnRemove${i} type="button">Remove</button>
+                <hr />
+            </li>`
+    }
+    ul.innerHTML = aux;
+    div.appendChild(ul)
+
+    document.querySelectorAll('.btnRemove').forEach((btn, index) => {
+        btn.addEventListener('click', () => {
+            removeBook(index)
+        })
+    })
 });
+
+function addBook(cTitle, cAuthor) {
+    const newBook = {
+        "title": cTitle,
+        "author": cAuthor
+    };
+    localStorageBooks.push(newBook);
+    localStorage.setItem('localStorageBooks', JSON.stringify(localStorageBooks));
+}
+
+function removeBook(index) {
+    localStorageBooks.splice(index, 1);
+    localStorage.setItem('localStorageBooks', JSON.stringify(localStorageBooks));
+    window.location.reload()
+
+}
+
+addForm.addEventListener('submit', () => {
+    addBook(addForm['title'].value, addForm['author'].value);
+});
+
+
+
+
