a9d0e2741de8b6aedec679ae4af932f484660679
Marcelo Alarcón <marceloalarconbarrenechea@gmail.com>
2023-01-16 15:14:14 -0400

    Add Dom manipulation

--- scripts.js
+++ scripts.js
@@ -1,24 +1,45 @@
 let books = [];
 
-function addBook(cTitle, cAuthor){
-    const o = {title: cTitle,
-               author: cAuthor};
+function addBook(cTitle, cAuthor) {
+    const o = {
+        title: cTitle,
+        author: cAuthor
+    };
     books.push(o);
 }
 
-function removeBook(cTitle){
-    const result = books.filter(book => book.title != cTitle );
+function removeBook(cTitle) {
+    const result = books.filter(book => book.title != cTitle);
     books = result;
 
     console.log(books);
 }
 
-addBook('Padre Rico Padre Pobre','Robert Kijosaki');
-addBook('Absalom, Absalom!','William Faulkner');
-addBook('A time to kill','John Grisham');
+addBook('Padre Rico Padre Pobre', 'Robert Kijosaki');
+addBook('Absalom, Absalom!', 'William Faulkner');
+addBook('A time to kill', 'John Grisham');
 
-for  (let i =0; i<books.length; i+=1){
+for (let i = 0; i < books.length; i += 1) {
     console.log(books[i]);
 }
 
 removeBook('Padre Rico Padre Pobre');
+
+/* -------------------------------------------------------------------------- */
+/*                              DOM MANIPULATION                              */
+/* -------------------------------------------------------------------------- */
+const div = document.querySelector('#bookListContainer')
+const ul = document.querySelector('#ulBookList')
+
+let aux = ''
+for (let i = 0; i < books.length; i++) {
+    aux += `<li>
+                <p>${books[i].title}</p>
+                <p>${books[i].author}</p>
+                <button id="btnRemove${i} type="button">Remove</button>
+                <hr />
+            </li>`
+
+}
+ul.innerHTML = aux;
+div.appendChild(ul)
