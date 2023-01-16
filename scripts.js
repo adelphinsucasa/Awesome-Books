2e001e8fdeb7970dca91cc24439c877b9ad53bd9
josedelphin <dalejandro.005@gmail.com>
2023-01-16 14:26:20 -0400

    Update index script and css

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
-console.log('Awesome Books');
+const btnAdd = document.getElementById('btnAdd');
+let aBooks = [];
+
+function addBook(cTitle, cAuthor){
+    const o = {title: cTitle,
+               author: cAuthor};
+    aBooks.push(o);
+}
+
+btnAdd.addEventListener('click',()=>{
+    console.log('Adding');
+    const txtTitle = document.getElementById('title');
+    const txtAuthor = document.getElementById('author');
+
+    console.log(txtTitle.textContent);
+    console.log(txtAuthor.textContent);
+});
