7e967e27bfda9746fe9bd90e0a2d39a9c7aafc6b
josedelphin <dalejandro.005@gmail.com>
2023-01-17 14:46:44 -0400

    Change script to use an instance of the class

---
+++ script.js
@@ -0,0 +1,71 @@
+const div = document.querySelector('#bookListContainer');
+const ul = document.querySelector('#ulBookList');
+const addForm = document.getElementById('addBookForm');
+
+class BookCollection {
+
+  constructor() {
+    this.books = []
+  }
+
+  addBook(cTitle, cAuthor) {
+    const newBook = {
+      title: cTitle,
+      author: cAuthor,
+    };
+    this.books.push(newBook);
+    localStorage.setItem('localStorageBooks', JSON.stringify(this.books));
+  }
+
+  removeBook(index) {
+    const result = this.books.filter(book => book.title != this.books[index].title)
+    this.books = result
+    localStorage.setItem('localStorageBooks', JSON.stringify(this.books));
+    window.location.reload();
+  }
+
+  getBooks() {
+    return this.books
+  }
+
+  setBooks(books){
+    this.books = books;
+  }
+}
+
+let newBooksCollection = new BookCollection()
+
+window.addEventListener('load', () => {
+
+  if (JSON.parse(localStorage.getItem('localStorageBooks')) === null) {
+    localStorage.setItem('localStorageBooks', JSON.stringify(newBooksCollection.getBooks()));
+  } else {
+    newBooksCollection.setBooks(JSON.parse(localStorage.getItem('localStorageBooks')));
+  }
+
+  /* -------------------------------------------------------------------------- */
+  /*                              DOM MANIPULATION                              */
+  /* -------------------------------------------------------------------------- */
+
+  let aux = '';
+  for (let i = 0; i < newBooksCollection.getBooks().length; i += 1) {
+    aux += `<li>
+                <p>${newBooksCollection.getBooks()[i].title}</p>
+                <p>${newBooksCollection.getBooks()[i].author}</p>
+                <button class="btnRemove" id="btnRemove${i} type="button">Remove</button>
+                <hr />
+            </li>`;
+  }
+  ul.innerHTML = aux;
+  div.appendChild(ul);
+
+  document.querySelectorAll('.btnRemove').forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      newBooksCollection.removeBook(index);
+    });
+  });
+});
+
+addForm.addEventListener('submit', () => {
+  newBooksCollection.addBook(addForm.title.value, addForm.author.value);
+});
